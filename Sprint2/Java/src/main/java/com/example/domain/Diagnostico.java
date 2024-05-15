3e9a39473da81803db6f6103c306cd12097802e3
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-14 21:06:25 -0300

    feat: add increment id

--- Sprint2/Java/src/main/java/com/example/domain/Diagnostico.java
+++ Sprint2/Java/src/main/java/com/example/domain/Diagnostico.java
@@ -2,17 +2,19 @@ package com.example.domain;
 
 import com.example.enums.VerificaDiagnostico;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Diagnostico {
 
+    public static int sequencia = 1;
     private int id;
     private String descricao;
     private Date data;
     private VerificaDiagnostico verificador;
 
     public Diagnostico(String descricao) {
-        id += 1;
+        id += sequencia++;
         this.descricao = descricao;
         this.data = new Date();
         this.verificador = VerificaDiagnostico.DIAGNOSTICO_NAO_RESOLVIDO;
@@ -32,5 +34,12 @@ public class Diagnostico {
 
     public void setVerificador(VerificaDiagnostico verificador) {
         this.verificador = verificador;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String dataStr = sdf.format(data);
+        return "id: " + id +  ", descrição: " + descricao + ", data: " + dataStr + ", verificador: " + verificador;
     }
 }
