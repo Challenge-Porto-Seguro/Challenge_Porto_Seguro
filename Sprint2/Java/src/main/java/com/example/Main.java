6262d8a04ff10e35e0cbc96afc49c2a30cbe420c
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-14 21:07:04 -0300

    test: testing methodo alterarSenha

--- Sprint2/Java/src/main/java/com/example/Main.java
+++ Sprint2/Java/src/main/java/com/example/Main.java
@@ -1,24 +1,36 @@
 package com.example;
 
-import com.example.domain.Endereco;
-import com.example.domain.Orcamento;
-import com.example.domain.Pedido;
-import com.example.domain.Produto;
+import com.example.domain.*;
+import com.example.domain.usuarios.Mecanico;
 import com.example.domain.usuarios.UsuarioComum;
 import com.example.domain.usuarios.UsuarioPorto;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         try {
             Endereco e = new Endereco("Elis Regina", 104, "05273-010", "Morro Doce", "São Paulo", "São Paulo");
             UsuarioComum uc = new UsuarioComum("Pedro", "12345678909", "ph@gmail.com", "12345678", e);
-            for(int i = 0; i < 4; i++){
+            for(int i = 0; i < 3; i++){
                 Orcamento o = new Orcamento();
                 o.addPedido(new Pedido(3, new Produto("Pedro", "de", 2)));
                 uc.addOrcamento(o);
-                System.out.println(uc.getQuantidadeOrcamento());
             }
+
+            System.out.println("Quantidade de orçamentos do Usuario Comum: " + uc.getQuantidadeOrcamento());
+
+            System.out.println("Alterar senha");
+            System.out.print("Digite seu cpf para confirmar que é você: ");
+            String cpf = sc.nextLine();
+
+            System.out.print("Digite a nova senha: ");
+            String novaSenha = sc.nextLine();
+            uc.alterarSenha(cpf, novaSenha);
+
 
         } catch (RuntimeException ex){
             System.out.println(ex.getMessage());
@@ -27,18 +39,34 @@ public class Main {
         try {
             Endereco e1 = new Endereco("Elis Regina", 104, "05273-010", "Morro Doce", "São Paulo", "São Paulo");
             UsuarioPorto up = new UsuarioPorto("Pedro", "12345678909", "ph@gmail.com", "12345678", e1, "1");
-            for(int i = 0; i < 8; i++){
+            for(int i = 0; i < 7; i++){
                 Orcamento o = new Orcamento();
                 o.addPedido(new Pedido(3, new Produto("Pedro", "de", 2)));
                 up.addOrcamento(o);
-                System.out.println(up.getQuantidadeOrcamento());
             }
+
+            System.out.println("\nQuantidade de orçamentos do Usuarios Porto: " + up.getQuantidadeOrcamento());
 
         } catch (RuntimeException ex){
             System.out.println(ex.getMessage());
         }
 
+        try {
+            Endereco e1 = new Endereco("Elis Regina", 104, "05273-010", "Morro Doce", "São Paulo", "São Paulo");
+            Mecanico mc = new Mecanico("Pedro", "12345678909", "ph@gmail.com", "12345678", e1, "Mecanica Ferraz");
+            for(int i = 0; i < 10; i++){
+                Orcamento o = new Orcamento();
+                o.addPedido(new Pedido(3, new Produto("Pedro", "de", 2)));
+                mc.addOrcamento(o);
+            }
 
+
+        } catch (RuntimeException ex){
+            System.out.println(ex.getMessage());
+        }
+
+        Diagnostico diagnostico = new Diagnostico("oi");
+        System.out.println(diagnostico);
 
     }
 }
