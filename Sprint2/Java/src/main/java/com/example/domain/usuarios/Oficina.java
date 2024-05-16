57f6d6fc7bb5991917f15c5e3925d9812b825038
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-15 23:24:56 -0300

    feat: class oficina

---
+++ Sprint2/Java/src/main/java/com/example/domain/usuarios/Oficina.java
@@ -0,0 +1,43 @@
+package com.example.domain.usuarios;
+
+import com.example.domain.Cnpj;
+import com.example.domain.Endereco;
+import com.example.domain.Orcamento;
+
+public class Oficina extends MinimoInformacao {
+
+    private String inscricaoEstadual;
+    private Cnpj cnpj;
+    private double valorAPagar;
+
+    public Oficina(String nome, String cnpj, String email, String senha, Endereco endereco, String inscricaoEstadual) {
+        super(nome, email, senha, endereco);
+        this.cnpj = new Cnpj(cnpj);
+        this.inscricaoEstadual = inscricaoEstadual;
+
+    }
+
+    public double getValorAPagar() {
+        return valorAPagar;
+    }
+
+    public Cnpj getCnpj() {
+        return cnpj;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    //Um oficina pode fazer quantos orçamento quiser porem paga um valor a cada orçamento
+    @Override
+    public void addOrcamento(Orcamento orcamento) {
+        getOrcamentos().add(orcamento);
+        valorAPagar += 5;
+    }
+
+    @Override
+    public String toString() {
+        return "Oficina: " + "Nome: " + getNome() + ", Email: " + getEmail() + cnpj;
+    }
+}
