8210f31aeacadb9247b19524b1541117e66e3a11
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-18 21:43:59 -0300

    refactor: add checks using the cep of the address

--- Sprint2/Java/src/main/java/com/example/domain/Endereco.java
+++ Sprint2/Java/src/main/java/com/example/domain/Endereco.java
@@ -1,22 +1,92 @@
 package com.example.domain;
 
-public class Endereco {
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Endereco {
+    @SerializedName("logradouro")
     private String rua;
     private int numero;
     private String cep;
     private String bairro;
+    @SerializedName("localidade")
     private String cidade;
-    private String estado;
+    private String uf;
 
-    public Endereco(String rua, int numero, String cep, String bairro, String cidade, String estado) {
-        verificaSeCepEValido(cep);
+    public Endereco(String rua, int numero, String cep, String bairro, String cidade, String uf) {
+        verificaSeInformacoesEstaoCerta(rua, numero, cep, bairro, cidade, uf);
         this.rua = rua;
         this.numero = numero;
         this.cep = cep;
         this.bairro = bairro;
         this.cidade = cidade;
-        this.estado = estado;
+        this.uf = uf;
+    }
+
+    public Endereco(String cep, int numero) {
+
+        Endereco endereco = pegaInformacoesEnderecoPorCep(cep);
+        if(endereco.cep == null) {
+            throw new RuntimeException("CEP invalido");
+        }
+        this.cep = endereco.cep;
+        this.rua = endereco.rua;
+        this.bairro = endereco.bairro;
+        this.cidade = endereco.cidade;
+        this.uf = endereco.uf;
+        this.numero = numero;
+    }
+
+    private void verificaSeInformacoesEstaoCerta(String rua, int numero, String cep, String bairro, String cidade, String uf){
+
+        Endereco endereco = pegaInformacoesEnderecoPorCep(cep);
+        if (endereco.cep == null){
+            throw new RuntimeException("CEP invalido");
+        }
+        if(!rua.equalsIgnoreCase(endereco.rua) ){
+            throw new RuntimeException("Rua invalida");
+        }
+        if(!bairro.equalsIgnoreCase(endereco.bairro) ){
+            throw new RuntimeException("Bairro invalido");
+        }
+        if(!cidade.equalsIgnoreCase(endereco.cidade)){
+            throw new RuntimeException("Cidade invalida");
+        }
+        if(!uf.equalsIgnoreCase(endereco.uf)){
+            throw new RuntimeException("Uf invalido");
+        }
+        if(numero < 0){
+            throw new RuntimeException("numero invalido");
+        }
+
+
+    }
+
+    private Endereco pegaInformacoesEnderecoPorCep(String cep){
+        verificaSeCepEValido(cep);
+        try {
+            String enderecoPesquisa = "http://viacep.com.br/ws/" + cep + "/json/";
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(enderecoPesquisa))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            Gson gson = new Gson();
+
+            return gson.fromJson(json, Endereco.class);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private void verificaSeCepEValido(String cep) {
@@ -46,7 +116,17 @@ public class Endereco {
         return cidade;
     }
 
-    public String getEstado() {
-        return estado;
+    public String getUf() {
+        return uf;
+    }
+
+}
+
+class teste{
+    public static void main(String[] args) {
+        Endereco endereco1 =new Endereco("05274-050", 104);
+        System.out.println(endereco1.getRua());
+        Endereco endereco = new Endereco("rua kenda", 104, "05274-050", "Jardim rosinha", "São Paulo", "sp");
+        System.out.println(endereco.getRua());
     }
 }
