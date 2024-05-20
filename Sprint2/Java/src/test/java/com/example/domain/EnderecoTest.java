3d62ebca3bef124db8a86f427e08bcc25e2222ba
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-19 22:27:58 -0300

    refactor: changing EnderecoTest class

--- Sprint2/Java/src/test/java/com/example/domain/EnderecoTest.java
+++ Sprint2/Java/src/test/java/com/example/domain/EnderecoTest.java
@@ -1,41 +1,50 @@
 package com.example.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-public class EnderecoTest {
+class EnderecoTest {
 
     Endereco nockEndereco = new Endereco("01311-000", 1106);
 
     @Test
-    public void testa_se_cep_for_01311000_rua_igual_avenida_paulista() {
-        Assert.assertEquals("Avenida Paulista", nockEndereco.getRua());
+    void testa_se_cep_for_01311000_rua_igual_avenida_paulista() {
+        Assertions.assertEquals("Avenida Paulista", nockEndereco.getRua());
     }
 
     @Test
-    public void testa_se_cep_for_01311000_e_numero_for_1106_numero_igual_1106() {
-        Assert.assertEquals(1106, nockEndereco.getNumero());
+    void testa_se_cep_for_01311000_e_numero_for_1106_numero_igual_1106() {
+        Assertions.assertEquals(1106, nockEndereco.getNumero());
     }
 
     @Test
-    public void testa_se_cep_for_01311000_cep_igual_01311000() {
-        Assert.assertEquals("01311-000", nockEndereco.getCep());
+    void testa_se_cep_for_01311000_cep_igual_01311000(){
+        Assertions.assertEquals("01311-000", nockEndereco.getCep());
     }
 
     @Test
-    public void testa_se_cep_for_01311000_bairro_igual_Bela_Vista() {
-        Assert.assertEquals("Bela Vista", nockEndereco.getBairro());
+    void testa_se_cep_for_01311000_bairro_igual_Bela_Vista() {
+        Assertions.assertEquals("Bela Vista", nockEndereco.getBairro());
     }
 
     @Test
-    public void testa_se_cep_for_01311000_cidade_igual_Sao_Paulo() {
-        Assert.assertEquals("São Paulo", nockEndereco.getCidade());
+    void testa_se_cep_for_01311000_cidade_igual_Sao_Paulo() {
+        Assertions.assertEquals("São Paulo", nockEndereco.getCidade());
     }
 
     @Test
-    public void testa_se_cep_for_01311000_uf_igual_sp() {
-        Assert.assertEquals("SP", nockEndereco.getUf());
+    void testa_se_cep_for_01311000_uf_igual_sp() {
+        Assertions.assertEquals("SP", nockEndereco.getUf());
+    }
+
+    @Test
+    void testa_se_cep_for_01311000_uf_igual_pa_retorna_erro() {
+        Assertions.assertThrows(RuntimeException.class,()-> new Endereco("Avenida Paulista", 1106, "01311-000", "Bela Vista", "São Paulo", "pa"));
+    }
+
+    @Test
+    void testa_se_cep_for_01311000_e_todas_iformacoes_corretas_endereco_criado(){
+        Assertions.assertDoesNotThrow(()-> new Endereco("Avenida Paulista", 1106, "01311-000", "Bela Vista", "São Paulo", "sp"));
     }
 
 }
