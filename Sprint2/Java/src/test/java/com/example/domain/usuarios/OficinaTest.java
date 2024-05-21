28ef0ebc79ef416eda0d9fad36d8e340f3b4466d
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-05-20 23:53:18 -0300

    refactor: changing OficinaTest class

--- Sprint2/Java/src/test/java/com/example/domain/usuarios/OficinaTest.java
+++ Sprint2/Java/src/test/java/com/example/domain/usuarios/OficinaTest.java
@@ -20,14 +20,14 @@ class OficinaTest {
     }
 
     @Test
-    void se_cnpj_igual_a_19036613000107_get_cnpj_igual_19036613000107() {
+    void se_cnpj_igual_a_19036613000107_get_cnpj_retorna_cnpj_formatado() {
         Oficina mockOficina = new Oficina("Mecanica Boa Viagem", "19036613000107", "bv@gmail.com", "12345678", mockEndereco, "12345678911");
         Cnpj mockCnpj = new Cnpj("19036613000107");
-        Assertions.assertEquals(mockCnpj, mockOficina.getCnpj());
+        Assertions.assertEquals(mockCnpj.toString(), mockOficina.getCnpj().toString());
     }
 
     @Test
-    void se_cnpj_igual_a_12345678911_get_cnpj_igual_12345678911() {
+    void se_inscricao_estadual_igual_12345678911_get_incricao_retorna_12345678911(){
         Oficina mockOficina = new Oficina("Mecanica Boa Viagem", "19036613000107", "bv@gmail.com", "12345678", mockEndereco, "12345678911");
         Assertions.assertEquals("12345678911", mockOficina.getInscricaoEstadual());
     }
