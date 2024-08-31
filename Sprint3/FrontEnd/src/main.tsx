c184c7b998d768d8614d71c8682d70ab4e2cd63b
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-08-31 16:27:44 -0300

    configurando rotas

--- Sprint3/FrontEnd/src/main.tsx
+++ Sprint3/FrontEnd/src/main.tsx
@@ -4,9 +4,44 @@ import App from './App.tsx'
 import './index.css'
 import GlobalConfig from './styled-globalConfig.ts'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import Home from "./routes/Home/index.tsx"
+import ChatBot from "./routes/ChatBot/index.tsx"
+import Login from "./routes/Login/index.tsx"
+import Colaboradores from "./routes/Colaboradores/index.tsx"
+import Error from './routes/Error/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/chatbot",
+        element: <ChatBot />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/colaboradores",
+        element: <Colaboradores />
+      }
+    ]
+
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
     <GlobalConfig/>
   </StrictMode>,
 )
