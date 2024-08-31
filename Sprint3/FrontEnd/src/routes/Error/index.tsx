195aae0eacbc90ba007324a7dd7943ee1cee3c7c
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-08-31 16:59:53 -0300

    add titulo a pagina de error e a estilizando

--- Sprint3/FrontEnd/src/routes/Error/index.tsx
+++ Sprint3/FrontEnd/src/routes/Error/index.tsx
@@ -1,8 +1,15 @@
+import { ErrorStyle } from "../../styledConfig"
+import { Link } from "react-router-dom"
+
 export default function Error() {
+    document.title = "Error"
     return(
-        <article>
-            <h1>Error 404 - NOT FOUND</h1>
-        </article>
+        <ErrorStyle>
+            <article>
+                <h1>Error 404 - NOT FOUND</h1>
+            </article>
+            <h3>Para voltar á pagina principal <Link to="/">Clique Aqui</Link></h3>
+        </ErrorStyle>
         
     )
 }
---
+++ Sprint3/FrontEnd/src/styledConfig.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+
+export const ErrorStyle = styled.main`
+    margin: 1em;
+    text-align: center;
+
+    article {
+        text-align: center;
+        border: 5px solid #8A8888;
+        padding: 1em;
+    }
+
+    h3 {
+        margin-top: 5em;
+    }
+
+    a{
+        text-decoration: none;
+        color: darkblue;
+    }
+
+    a:hover {
+        color: blue;
+    }
+    
+`   
