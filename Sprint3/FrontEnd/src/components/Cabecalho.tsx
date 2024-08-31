99da1b6004bfabcabfd69d7c8e66f43d302f2b07
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-08-31 17:23:58 -0300

    movendo estilização para um arquivo separado

--- Sprint3/FrontEnd/src/components/Cabecalho.tsx
+++ Sprint3/FrontEnd/src/components/Cabecalho.tsx
@@ -1,49 +1,8 @@
 import logo_porto from "./../assets/logo_porto.webp"
 import colaboradores from "./../assets/colaboradores.png"
 import chatbot from "./../assets/iconeChatBot.png"
-import styled from "styled-components"
+import { HeaderStyle } from "../styledConfig"
 import { Link } from "react-router-dom"
-
-const HeaderStyle = styled.header`
-    border-bottom: 2px solid #8A8888;
-    padding: 0.2em 0;
-    color: var(--primary-color);
-
-    .header__lista{
-        display: flex;
-        align-items: center;
-        gap: 50px;
-    }
-
-    .header__lista--item {
-        width: 25%;
-        text-align: center;
-        list-style: none;
-    }
-
-    .header__link{
-        text-decoration: none;
-        color: var(--primary-color);
-    }
-
-
-    .header__logo {
-        width: 70%;
-    }   
-
-    .header__paginas--img {
-        width: 20%;
-    }
-
-    .header__botao {
-        border: 2px solid black;
-        text-align: center;
-        padding: 1em 2em;
-        width: 15em;
-        margin: 0 1em;
-    }
-
-`
 
 export default function Cabecalho() {
 
--- Sprint3/FrontEnd/src/styledConfig.ts
+++ Sprint3/FrontEnd/src/styledConfig.ts
@@ -1,5 +1,46 @@
 import styled from "styled-components"
 
+export const HeaderStyle = styled.header`
+    border-bottom: 2px solid #8A8888;
+    padding: 0.2em 0;
+    color: var(--primary-color);
+
+    .header__lista{
+        display: flex;
+        align-items: center;
+        gap: 50px;
+    }
+
+    .header__lista--item {
+        width: 25%;
+        text-align: center;
+        list-style: none;
+    }
+
+    .header__link{
+        text-decoration: none;
+        color: var(--primary-color);
+    }
+
+
+    .header__logo {
+        width: 70%;
+    }   
+
+    .header__paginas--img {
+        width: 20%;
+    }
+
+    .header__botao {
+        border: 2px solid black;
+        text-align: center;
+        padding: 1em 2em;
+        width: 15em;
+        margin: 0 1em;
+    }
+
+`
+
 export const ErrorStyle = styled.main`
     margin: 1em;
     text-align: center;
@@ -23,4 +64,4 @@ export const ErrorStyle = styled.main`
         color: blue;
     }
     
-`   
+` 
