ee11c58495ea4bc3274acfeffb3ee27331ba87be
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-08-31 16:28:33 -0300

    alterando o component cabecalho para linkar com as rotas

--- Sprint3/FrontEnd/src/components/Cabecalho.tsx
+++ Sprint3/FrontEnd/src/components/Cabecalho.tsx
@@ -2,6 +2,7 @@ import logo_porto from "./../assets/logo_porto.webp"
 import colaboradores from "./../assets/colaboradores.png"
 import chatbot from "./../assets/iconeChatBot.png"
 import styled from "styled-components"
+import { Link } from "react-router-dom"
 
 const HeaderStyle = styled.header`
     border-bottom: 2px solid #8A8888;
@@ -51,21 +52,21 @@ export default function Cabecalho() {
             <nav>
                 <ul className="header__lista">
                     <li className="header__lista--item">
-                        <a className="header__link logo" href="#"><img className="header__logo" src={logo_porto} alt="Logo da Porto" /></a>
+                        <Link className="header__link logo" to="/"><img className="header__logo" src={logo_porto} alt="Logo da Porto" /></Link>
                     </li>
                     <li className="header__lista--item"> 
-                        <a className="header__link header__paginas" href="#">
+                        <Link className="header__link header__paginas" to="/chatbot">
                             <img className="header__paginas--img" src={chatbot} alt="logo do chatbot" />
                             <h3>ChatBot</h3>
-                        </a>
+                        </Link>
                     </li>
                     <li className="header__lista--item">
-                        <a className="header__link header__paginas" href="#">
+                        <Link className="header__link header__paginas" to="colaboradores">
                             <img className="header__paginas--img" src={colaboradores} alt="logo de colaboradores" />
                             <h3>Colaboradores</h3>
-                        </a>
+                        </Link>
                     </li>
-                    <li className="header__lista--item"><a className="header__botao header__link" href="">Area do Cliente</a></li>
+                    <li className="header__lista--item"><Link className="header__botao header__link" to="/login">Area do Cliente</Link></li>
                 </ul>
             </nav>
         </HeaderStyle>
