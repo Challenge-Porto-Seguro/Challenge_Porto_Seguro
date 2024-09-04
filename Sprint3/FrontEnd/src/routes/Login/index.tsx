e9c7dccfd1bb1e8eee8d0e81a7ae79bad04e18d1
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-04 14:05:36 -0300

    update input types

--- Sprint3/FrontEnd/src/routes/Login/index.tsx
+++ Sprint3/FrontEnd/src/routes/Login/index.tsx
@@ -13,10 +13,10 @@ export default function Login() {
                 </article>
                 <article>
                     <h2 className="nome__input">Digite seu e-mail:</h2>
-                    <input className="input" type="text" placeholder="E-mail" />
+                    <input className="input" type="email" placeholder="E-mail" />
 
                     <h2 className="nome__input">Digite sua senha:</h2>
-                    <input className="input" type="text" placeholder="Senha" />
+                    <input className="input" type="password" placeholder="Senha" />
                     <p className="esqueceu__senha">Esqueceu sua senha?</p>
                     <div className="div__botao">
                         <button className="btn">Entrar</button>
