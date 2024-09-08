6cea66e4d4d9bfdb87222561b61d94767e893fbe
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-08 18:44:09 -0300

    refactor: alterando erros

--- Sprint3/Java/src/main/java/com/example/validacoes/VerificaSeInformacoesEnderecoValida.java
+++ Sprint3/Java/src/main/java/com/example/validacoes/VerificaSeInformacoesEnderecoValida.java
@@ -7,19 +7,19 @@ public class VerificaSeInformacoesEnderecoValida {
 
     public static void verificaInformacoes(String rua, int numero, String cep, String bairro, String cidade, String uf){
         Endereco endereco = PegaEndereceoViaApi.pegarEndereco(cep);
-        if (endereco.cep == null){
+        if (endereco.getCep() == null){
             throw new RuntimeException("CEP invalido");
         }
-        if(!rua.equalsIgnoreCase(endereco.rua) ){
+        if(!rua.equalsIgnoreCase(endereco.getRua()) ){
             throw new RuntimeException("Rua invalida");
         }
-        if(!bairro.equalsIgnoreCase(endereco.bairro) ){
+        if(!bairro.equalsIgnoreCase(endereco.getBairro()) ){
             throw new RuntimeException("Bairro invalido");
         }
-        if(!cidade.equalsIgnoreCase(endereco.cidade)){
+        if(!cidade.equalsIgnoreCase(endereco.getCidade())){
             throw new RuntimeException("Cidade invalida");
         }
-        if(!uf.equalsIgnoreCase(endereco.uf)){
+        if(!uf.equalsIgnoreCase(endereco.getUf())){
             throw new RuntimeException("Uf invalido");
         }
         if(numero < 0){
