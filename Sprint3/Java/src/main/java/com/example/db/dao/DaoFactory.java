893cf6619779a32667152bd41a3f3ff930ed7fa1
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-08 18:49:52 -0300

    feat: add usuarioDao

--- Sprint3/Java/src/main/java/com/example/db/dao/DaoFactory.java
+++ Sprint3/Java/src/main/java/com/example/db/dao/DaoFactory.java
@@ -2,10 +2,15 @@ package com.example.db.dao;
 
 import com.example.db.DB;
 import com.example.db.implDao.AutomovelDaoJDBC;
+import com.example.db.implDao.UsuarioDaoJDBC;
 
 public class DaoFactory {
 
     public static AutomovelDao createAutomovelDao() {
         return new AutomovelDaoJDBC(DB.getConnection());
+    }
+
+    public static UsuarioDao createUsuarioDao() {
+        return new UsuarioDaoJDBC(DB.getConnection());
     }
 }
