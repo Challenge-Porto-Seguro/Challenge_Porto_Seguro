c4894db67ded9c872f7c22d39f247c982b4dded6
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-08 18:50:44 -0300

    feat: testando insert e get by id de usuarios

--- Sprint3/Java/src/main/java/com/example/Teste.java
+++ Sprint3/Java/src/main/java/com/example/Teste.java
@@ -3,23 +3,31 @@ package com.example;
 import com.example.db.dao.AutomovelDao;
 import com.example.db.dao.DaoFactory;
 import com.example.model.Automovel;
+import com.example.model.usuarios.Usuario;
+import com.example.service.UsuarioService;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class Teste {
     public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        AutomovelDao automovelDao = DaoFactory.createAutomovelDao();
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+//        AutomovelDao automovelDao = DaoFactory.createAutomovelDao();
+//
+//        System.out.println("Teste 2");
+//        System.out.println(automovelDao.findById(1));
+//
+//        System.out.println("Teste 3");
+//        Automovel automovel1 = automovelDao.findById(1).get();
+//        System.out.println(automovel1);
+//        automovel1.setMarca("Ola mundo");
+//        automovelDao.update(automovel1);
+//        System.out.println(automovelDao.findById(1).get());
 
-        System.out.println("Teste 2");
-        System.out.println(automovelDao.findById(1));
+        UsuarioService usuarioService = new UsuarioService();
+//        Usuario usuario = new Usuario("Pedro", "12345678909", "ph6704938@gmail.com", "123456789");
+//        usuarioService.cadastraUsuario(usuario);
 
-        System.out.println("Teste 3");
-        Automovel automovel1 = automovelDao.findById(1).get();
-        System.out.println(automovel1);
-        automovel1.setMarca("Ola mundo");
-        automovelDao.update(automovel1);
-        System.out.println(automovelDao.findById(1).get());
+        System.out.println(usuarioService.buscaUsuarioPorId(28L));
     }
 }
