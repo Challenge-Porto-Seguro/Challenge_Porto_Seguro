49dbc7158f019dc0d505a9cecaefd39e66ffbd82
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-08 23:59:01 -0300

    feat: testando metodo de cadastrar automovel

--- Sprint3/Java/src/main/java/com/example/Teste.java
+++ Sprint3/Java/src/main/java/com/example/Teste.java
@@ -1,33 +1,24 @@
 package com.example;
 
-import com.example.db.dao.AutomovelDao;
-import com.example.db.dao.DaoFactory;
 import com.example.model.Automovel;
-import com.example.model.usuarios.Usuario;
+import com.example.service.AutomovelService;
 import com.example.service.UsuarioService;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Teste {
     public static void main(String[] args) throws ParseException {
-//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-//        AutomovelDao automovelDao = DaoFactory.createAutomovelDao();
-//
-//        System.out.println("Teste 2");
-//        System.out.println(automovelDao.findById(1));
-//
-//        System.out.println("Teste 3");
-//        Automovel automovel1 = automovelDao.findById(1).get();
-//        System.out.println(automovel1);
-//        automovel1.setMarca("Ola mundo");
-//        automovelDao.update(automovel1);
-//        System.out.println(automovelDao.findById(1).get());
 
         UsuarioService usuarioService = new UsuarioService();
 //        Usuario usuario = new Usuario("Pedro", "12345678909", "ph6704938@gmail.com", "123456789");
 //        usuarioService.cadastraUsuario(usuario);
 
         System.out.println(usuarioService.buscaUsuarioPorId(28L));
+
+        AutomovelService automovelService = new AutomovelService();
+        Automovel automovel = new Automovel("Tesla", "model x", "ABC1234", new Date(), usuarioService.buscaUsuarioPorId(28L));
+        automovelService.cadastraAutomovel(automovel);
+
     }
 }
