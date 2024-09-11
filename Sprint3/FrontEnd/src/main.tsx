4b071dd248aa9d48626a5beaab46e1c8e3774dfe
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-10 22:49:53 -0300

    adicionando rotas

--- Sprint3/FrontEnd/src/main.tsx
+++ Sprint3/FrontEnd/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import GlobalConfig from './styled-globalConfig.ts'
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
@@ -11,6 +10,8 @@ import ChatBot from "./routes/ChatBot/index.tsx"
 import Login from "./routes/Login/index.tsx"
 import Colaboradores from "./routes/Colaboradores/index.tsx"
 import Error from './routes/Error/index.tsx'
+import CadastroUsuario from './routes/CadastroUsuario/index.tsx'
+import CadastroAutomovel from './routes/CadastroAutomovel/index.tsx'
 
 const router = createBrowserRouter([
   {
@@ -33,6 +34,14 @@ const router = createBrowserRouter([
       {
         path: "/colaboradores",
         element: <Colaboradores />
+      },
+      {
+        path: "/cadastro/usuario",
+        element: <CadastroUsuario />
+      },
+      {
+        path: "/cadastro/automovel",
+        element: <CadastroAutomovel />
       }
     ]
 
