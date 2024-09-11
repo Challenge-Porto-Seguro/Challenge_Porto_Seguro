5d0566c3cce8a70dc5d593a02f00763507728108
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-10 22:50:20 -0300

    add chamada para cadastrar um usuario

--- Sprint3/FrontEnd/src/routes/Login/index.tsx
+++ Sprint3/FrontEnd/src/routes/Login/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import SetaVoltar from "../../components/SetaVoltar"
 import { LoginPageStyle } from "../../styledConfig"
 import CamposLogin from "./CamposLogin"
@@ -11,6 +12,7 @@ export default function Login() {
                 <article className="container__titulos">
                     <h1 className="container__titulos--titulo">Acesse</h1>
                     <h3 className="container__titulos--subtitulo">com e-mail e senha</h3>
+                    <Link to="/cadastro/usuario">Não tem conta clique aqui para cadastrar</Link>
                 </article>
                 <CamposLogin />
             </section>
