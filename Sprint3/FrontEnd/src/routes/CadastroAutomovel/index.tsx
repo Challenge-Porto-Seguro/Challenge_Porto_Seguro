103f006cc8fc071944aabf3ca46ef50bd254adcb
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-10 22:48:39 -0300

    criando pagina de cadastro de veiculo e cadastro de usuario

---
+++ Sprint3/FrontEnd/src/routes/CadastroAutomovel/index.tsx
@@ -0,0 +1,26 @@
+import Formulario from "../../components/Formulario"
+import { CadastroPageStyle, FormularioCadastroVeiculo } from "../../styledConfig"
+import { CriacaoFormulario } from "../../types"
+
+export default function CadastroAutomovel() {
+
+    const formularioCadastroVeiculo: CriacaoFormulario[] = [
+        {id: "marca", nomeCampo: "Marca", tipo: "text", placeHolder: "Digite a marca de seu veiculo"},
+        {id: "modelo", nomeCampo: "Modelo", tipo: "text", placeHolder: "Digite o modelo do seu veiculo"},
+        {id: "ano", nomeCampo: "Ano", tipo: "date", placeHolder: "Digite o ano de seu veiculo"},
+        {id: "placa", nomeCampo: "Placa", tipo: "text", placeHolder: "Digite a placa do seu veiculo"}
+    ]
+    return (
+        <CadastroPageStyle>
+            <h1>Cadastro de veiculo</h1>
+            <FormularioCadastroVeiculo>
+                {
+                    formularioCadastroVeiculo.map((campo, index) => (
+                        <Formulario key={index} id={campo.id} nomeCampo={campo.nomeCampo} tipo={campo.tipo} placeHolder={campo.placeHolder} />
+                    ))
+                }
+                <button className="botao_cadastro">Cadastrar</button>
+            </FormularioCadastroVeiculo>
+        </CadastroPageStyle>
+    )
+}
---
+++ Sprint3/FrontEnd/src/routes/CadastroUsuario/index.tsx
@@ -0,0 +1,53 @@
+import Formulario from "../../components/Formulario";
+import { CadastroPageStyle, FormularioCadastroStyle } from "../../styledConfig";
+import { CriacaoFormulario } from "../../types";
+
+export default function CadastroUsuario() {
+
+
+    const listaCampos: CriacaoFormulario[] = [
+        {id: "nome", nomeCampo: "Nome", tipo: "text", placeHolder: "Digite seu nome"},
+        {id: "email", nomeCampo: "Email", tipo: "email", placeHolder: "Digite seu email"},
+        {id: "senha", nomeCampo: "Senha", tipo: "password", placeHolder: "Digite seu email"},
+        {id: "confirma_senha", nomeCampo: "Confirmar Senha", tipo: "password", placeHolder: "confirme sua senha"},
+        {id: "bairro", nomeCampo: "Bairro", tipo: "text", placeHolder: "Digite seu bairro"},
+        {id: "cidade", nomeCampo: "Cidade", tipo: "text", placeHolder: "Digite sua cidade"},
+        {id: "estado", nomeCampo: "Estado", tipo: "text", placeHolder: "Digite seu estado"}
+    ]
+
+    const listaEnderecoContainer: CriacaoFormulario[] = [
+        {id: "rua", nomeCampo: "Rua", tipo: "text", placeHolder: "Digite o nome da sua rua"},
+        {id: "numero", nomeCampo: "Numero", tipo: "number", placeHolder: ""},
+    ]
+
+    return (
+        <CadastroPageStyle>
+
+            <h1>Cadastro de Usuario</h1>
+            <FormularioCadastroStyle>
+
+                {
+                    listaCampos.map((campo, index) => (
+                        index - 1 === 3 ? 
+                        <div className="endereco_container">
+                        {
+                            listaEnderecoContainer.map((end, index) => (
+                                <div className={end.id}>
+                                    <Formulario key={index} id={end.id} nomeCampo={end.nomeCampo} tipo={end.tipo} placeHolder={end.placeHolder} />
+                                </div>
+                            ))
+                        }
+                        </div> 
+                         :  
+                        <Formulario key={index} id={campo.id} nomeCampo={campo.nomeCampo} tipo={campo.tipo} placeHolder={campo.placeHolder} />
+                    ))
+                }
+
+                <button className="botao_cadastro">Cadastrar</button>
+                
+            </FormularioCadastroStyle>
+
+            
+        </CadastroPageStyle>
+    )
+}
