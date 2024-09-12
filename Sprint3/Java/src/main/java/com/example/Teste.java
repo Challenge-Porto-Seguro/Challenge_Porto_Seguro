e5388fe2f2cc21ea368f04a77943f7936f5fcf24
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-09-11 22:32:05 -0300

    feat: testando

--- Sprint3/Java/src/main/java/com/example/Teste.java
+++ Sprint3/Java/src/main/java/com/example/Teste.java
@@ -12,13 +12,22 @@ public class Teste {
     public static void main(String[] args) throws ParseException {
 
         UsuarioService usuarioService = new UsuarioService();
-//        Usuario usuario = new Usuario("Pedro", "12345678909", "ph6704938@gmail.com", "123456789");
+        Usuario usuario = new Usuario("Pedro", "12345678909", "ph6704938@gmail.com", "123456789");
 //        usuarioService.cadastraUsuario(usuario);
 
         System.out.println(usuarioService.buscaUsuarioPorId(1L));
+        usuario = usuarioService.buscaUsuarioPorId(1L);
+        usuario.setEmail("pedro@gmail.com");
+        usuarioService.alteraUsuario(usuario);
+
+        System.out.println(usuarioService.buscaUsuarioPorId(1L));
+
+        usuario.setNome("Joao");
+        usuarioService.alteraUsuario(usuario);
+        System.out.println(usuarioService.buscaUsuarioPorId(1L));
 
         AutomovelService automovelService = new AutomovelService();
-//        Automovel automovel = new Automovel("Tesla", "model x", "ABC1234", new Date(), usuarioService.buscaUsuarioPorId(1L));
+        Automovel automovel = new Automovel("Tesla", "model x", "ABC1234", new Date(), usuarioService.buscaUsuarioPorId(1L));
 //        automovelService.cadastraAutomovel(automovel);
 
         System.out.println(automovelService.buscaAutomovelPorId(1L));
