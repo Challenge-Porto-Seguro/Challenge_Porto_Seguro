20f87fd7abee2a28c1dcfc5921c8baf74b7ab75f
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-25 19:53:27 -0300

    feat: registrando filtro

--- Sprint4/Java/src/main/java/com/example/App.java
+++ Sprint4/Java/src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.config.CorsFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
@@ -9,5 +10,6 @@ public class App extends ResourceConfig {
 
     public App() {
         packages("com.example");
+        register(CorsFilter.class);
     }
 }
