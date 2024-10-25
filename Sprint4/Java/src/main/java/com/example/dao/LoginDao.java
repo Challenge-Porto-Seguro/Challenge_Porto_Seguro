8b8110a608091f482e5bfe27695ba8ea89843bff
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-25 10:40:56 -0300

    feat: add metodo para atualizar login

--- Sprint4/Java/src/main/java/com/example/dao/LoginDao.java
+++ Sprint4/Java/src/main/java/com/example/dao/LoginDao.java
@@ -1,6 +1,8 @@
 package com.example.dao;
 
 import com.example.exceptions.LoginNotCreate;
+import com.example.exceptions.LoginNotFound;
+import com.example.exceptions.LoginNotUpdade;
 import com.example.model.Login;
 
 import java.sql.Connection;
@@ -8,4 +10,5 @@ import java.sql.Connection;
 public interface LoginDao {
 
     Long insertLogin(Connection conn, Login login) throws LoginNotCreate;
+    void updateLogin(Connection conn, Login login) throws LoginNotFound;
 }
--- Sprint4/Java/src/main/java/com/example/dao/LoginDaoImpl.java
+++ Sprint4/Java/src/main/java/com/example/dao/LoginDaoImpl.java
@@ -1,6 +1,8 @@
 package com.example.dao;
 
 import com.example.exceptions.LoginNotCreate;
+import com.example.exceptions.LoginNotFound;
+import com.example.exceptions.LoginNotUpdade;
 import com.example.model.Login;
 
 import java.sql.Connection;
@@ -38,5 +40,25 @@ final class LoginDaoImpl implements LoginDao{
             this.logger.warning("Erro ao inserir login: " + e.getMessage());
         }
         return loginId;
+    }
+
+    @Override
+    public void updateLogin(Connection conn, Login login) throws LoginNotFound {
+        String sql = """
+                update T_PS_PESSOA
+                set nm_nome = ?, sq_senha = ?
+                where cd_pessoa = ?
+            """;
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, login.getNome());
+            ps.setString(2, login.getSenha());
+            ps.setLong(3, login.getId());
+            int rows = ps.executeUpdate();
+            if (rows != 1) {
+                throw new LoginNotFound();
+            }
+        } catch (SQLException e) {
+            this.logger.warning("Erro ao atualizar login: " + e.getMessage());
+        }
     }
 }
