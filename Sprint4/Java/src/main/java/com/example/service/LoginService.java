2fc4e85e56dccec38ba513cde63008046bbcf328
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-25 12:18:33 -0300

    feat: add metodo de cadastrar e atualizar login

--- Sprint4/Java/src/main/java/com/example/service/LoginService.java
+++ Sprint4/Java/src/main/java/com/example/service/LoginService.java
@@ -1,9 +1,15 @@
 package com.example.service;
 
+import com.example.exceptions.LoginNotCreate;
 import com.example.exceptions.LoginNotFound;
+import com.example.exceptions.LoginNotUpdade;
 import com.example.model.Login;
 
 public interface LoginService {
 
     Long login(Login login) throws LoginNotFound;
+
+    void cadastrar(Login login) throws LoginNotCreate;
+
+    void update(Login login) throws LoginNotFound, LoginNotUpdade;
 }
--- Sprint4/Java/src/main/java/com/example/service/LoginServiceImpl.java
+++ Sprint4/Java/src/main/java/com/example/service/LoginServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.service;
 import com.example.config.DatabaseConnectionFactory;
 import com.example.dao.LoginDao;
 import com.example.dao.LoginDaoFactory;
+import com.example.exceptions.LoginNotCreate;
 import com.example.exceptions.LoginNotFound;
+import com.example.exceptions.LoginNotUpdade;
 import com.example.model.Login;
 
 import java.sql.Connection;
@@ -19,6 +21,25 @@ final class LoginServiceImpl implements LoginService {
             return loginDao.logar(connection, login);
         } catch (SQLException e) {
             throw new LoginNotFound();
+        }
+    }
+
+    @Override
+    public void cadastrar(Login login) throws LoginNotCreate {
+        try(Connection connection = DatabaseConnectionFactory.getConnection()) {
+            loginDao.insertLogin(connection, login);
+            connection.commit();
+        } catch (SQLException e) {
+            throw new LoginNotCreate();
+        }
+    }
+
+    @Override
+    public void update(Login login) throws LoginNotFound, LoginNotUpdade {
+        try(Connection connection = DatabaseConnectionFactory.getConnection()) {
+            loginDao.updateLogin(connection, login);
+        } catch (SQLException e) {
+            throw new LoginNotUpdade();
         }
     }
 }
