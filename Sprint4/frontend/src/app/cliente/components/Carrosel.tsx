f6e4d5c60f43b2cf992c67989d4530b1ecc0bc13
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-29 22:35:25 -0300

    fix: arrumando link e passando id para o CardCarro

--- Sprint4/frontend/src/app/cliente/components/Carrosel.tsx
+++ Sprint4/frontend/src/app/cliente/components/Carrosel.tsx
@@ -23,13 +23,13 @@ export default function Carrosel({ carros }: { carros: Carro[] }) {
                     style={{ transform: `translateX(-${atual * (100 / carrosPorPagina)}%)` }}>
                     {carros.map((c) => (
                         <div key={c.id} className="flex-none w-1/3 px-2">
-                            <CardCarros placa={c.placa} marca={c.marca} modelo={c.modelo} data={c.data} />
+                            <CardCarros id={c.id} placa={c.placa} marca={c.marca} modelo={c.modelo} data={c.data} />
                         </div>
                     ))}
                 </div>
-                <div className="absolute inset-0 flex items-center justify-between p-4">
-                    <button className="text-3xl font-black pb-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white" onClick={prev}>{" < "}</button>
-                    <button className="text-3xl font-black pb-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white" onClick={next}>{" > "}</button>
+                <div className="absolute inset-0 flex items-center justify-between p-4 pointer-events-none">
+                    <button className="text-3xl font-black pb-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white pointer-events-auto" onClick={prev}>{" < "}</button>
+                    <button className="text-3xl font-black pb-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white pointer-events-auto" onClick={next}>{" > "}</button>
                 </div>
                 <div className="absolute bottom-4 right-0 left-0">
                     <div className="flex items-center justify-center gap-2">
@@ -37,7 +37,7 @@ export default function Carrosel({ carros }: { carros: Carro[] }) {
                             <div key={i} onClick={() => setAtual(i)} className={`transition-all w-3 h-3 bg-indigo-700 rounded-full ${atual === i ? "p-2" : "bg-opacity-5"}`}>
                             </div>
                         ))}
-                    </div>
+                    </div>  
                 </div>
             </div>
         </div>
