43c9fa9293648c24f33cd3b715282f2421af3c57
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-31 20:22:02 -0300

    feat: atualizando para ter um codigo para adicioanar no banco como a e i

--- Sprint4/Java/src/main/java/com/example/model/VerificaDiagnostico.java
+++ Sprint4/Java/src/main/java/com/example/model/VerificaDiagnostico.java
@@ -2,6 +2,25 @@ package com.example.model;
 
 public enum VerificaDiagnostico {
 
-    DIAGNOSTICO_NAO_RESOLVIDO,
-    RESOLVIDO
+    DIAGNOSTICO_NAO_RESOLVIDO("A"),
+    RESOLVIDO("I");
+
+    private String codigo;
+
+    VerificaDiagnostico(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public static VerificaDiagnostico getDiagnostico(String codigo) {
+        for (VerificaDiagnostico diagnostico : VerificaDiagnostico.values()) {
+            if (diagnostico.getCodigo().equals(codigo)) {
+                return diagnostico;
+            }
+        }
+        return null;
+    }
 }
