3b741ae15808a81e4f3e0f2f7c53d49b11caab52
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-10-31 23:28:55 -0300

    feat: criando erro de endereco

---
+++ Sprint4/Java/src/main/java/com/example/exceptions/EnderecoNotCreate.java
@@ -0,0 +1,7 @@
+package com.example.exceptions;
+
+public class EnderecoNotCreate extends Exception {
+    public EnderecoNotCreate(String message) {
+        super(message);
+    }
+}
---
+++ Sprint4/Java/src/main/java/com/example/exceptions/EnderecoNotUpdate.java
@@ -0,0 +1,8 @@
+package com.example.exceptions;
+
+public class EnderecoNotUpdate extends Exception {
+
+    public EnderecoNotUpdate(String message) {
+        super(message);
+    }
+}
