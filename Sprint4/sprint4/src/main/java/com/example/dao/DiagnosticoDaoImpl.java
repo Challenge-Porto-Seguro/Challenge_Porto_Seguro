517bdf6e1c2c079a179cebf51cad9654b71e8e90
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-11-02 18:13:52 -0300

    feat: create methodo de deletar diagnostico

--- Sprint4/sprint4/src/main/java/com/example/dao/DiagnosticoDaoImpl.java
+++ Sprint4/sprint4/src/main/java/com/example/dao/DiagnosticoDaoImpl.java
@@ -50,7 +50,7 @@ final class DiagnosticoDaoImpl implements DiagnosticoDao {
 
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
-                    Diagnostico diagnostico = instanciaDiagnostico(rs);
+                    Diagnostico diagnostico = InstanciaObjetos.instanciaDiagnostico(rs);
                     return Optional.of(diagnostico);
                 }
             }
@@ -92,7 +92,7 @@ final class DiagnosticoDaoImpl implements DiagnosticoDao {
             ps.setLong(1, cdPessoa);
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
-                    diagnosticos.add(instanciaDiagnostico(rs));
+                    diagnosticos.add(InstanciaObjetos.instanciaDiagnostico(rs));
 
                 }
             }
@@ -100,30 +100,18 @@ final class DiagnosticoDaoImpl implements DiagnosticoDao {
         }
     }
 
-    private Usuario instaciaUsuario(ResultSet rs) throws SQLException {
-        Usuario usuario = new Usuario(rs.getString("nm_nome"), rs.getString("sq_cpf"), rs.getString("nm_email"), rs.getString("sq_senha"));
-        usuario.setId(rs.getLong("cd_pessoa"));
-        return usuario;
-    }
+    @Override
+    public void deleteDiagnostico(Long id, Connection connection) throws SQLException, DiagnosticoNotFound {
+        final String sql = """
+        DELETE FROM T_PS_DIAGNOSTICO WHERE cd_diagnostico = ?
+    """;
 
-    private Automovel instanciaAutomovel(ResultSet rs) throws SQLException {
-        Automovel automovel = new Automovel(rs.getString("nm_marca_veiculo"), rs.getString("nm_modelo_veiculo"), rs.getString("sq_placa"), rs.getDate("dt_veiculo").toLocalDate(), instaciaUsuario(rs));
-        automovel.setId(rs.getLong("cd_automovel"));
-        return automovel;
-    }
-
-    private Diagnostico instanciaDiagnostico(ResultSet rs) throws SQLException {
-        Diagnostico diagnostico = new Diagnostico(rs.getString("nm_descricao_diagnostico"), rs.getDate("dt_inicio_diagnostico").toLocalDate(), VerificaDiagnostico.getDiagnostico(rs.getString("st_diagnostico")));
-        diagnostico.setId(rs.getLong("cd_diagnostico"));
-        diagnostico.setOficina(instaciaOficina(rs));
-        diagnostico.setAutomovel(instanciaAutomovel(rs));
-        diagnostico.setDataFinalizado(rs.getDate("dt_fim_diagnostico").toLocalDate());
-        return diagnostico;
-    }
-
-    private Oficina instaciaOficina(ResultSet rs) throws SQLException {
-        Oficina oficina = new Oficina(rs.getString("nm_nome"), rs.getString("sq_cnpj"), rs.getString("nm_email"), rs.getString("sq_senha"), rs.getString("sq_inscricao_estadual"));
-        oficina.setId(rs.getLong("cd_oficina"));
-        return oficina;
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setLong(1, id);
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new DiagnosticoNotFound();
+            }
+        }
     }
 }
