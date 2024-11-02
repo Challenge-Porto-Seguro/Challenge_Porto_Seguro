f942283eb819284b310d5ce7139d82a89cb30ea0
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-11-02 18:06:12 -0300

    fix: removendo erros não ultilizando de login

--- Sprint4/sprint4/src/main/java/com/example/service/LoginService.java
+++ Sprint4/sprint4/src/main/java/com/example/service/LoginService.java
@@ -4,13 +4,12 @@ import com.example.exceptions.*;
 import com.example.model.Login;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 
 public interface LoginService {
 
     Long login(String email, String senha) throws LoginNotFound, ErroLogar;
 
-    void cadastrar(Connection connection, Login login) throws LoginNotCreate, CadastroInvalido, SQLException;
+    void cadastrar(Connection connection, Login login) throws LoginNotCreate, CadastroInvalido;
 
-    void update(Connection connection, Login login) throws LoginNotFound, LoginNotUpdade, SQLException;
+    void update(Connection connection, Login login) throws LoginNotFound, LoginNotUpdade;
 }
