c75ab3e5671f337769cb56bea5ffc1e1d533de4e
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-11-02 18:08:38 -0300

    feat: add codigo construtor e metodo para buscar o status atraves do codigo

--- Sprint4/sprint4/src/main/java/com/example/model/StatusOrcamento.java
+++ Sprint4/sprint4/src/main/java/com/example/model/StatusOrcamento.java
@@ -1,6 +1,25 @@
 package com.example.model;
 
 public enum StatusOrcamento {
-    ATIVO,
-    INATIVO
+    ATIVO("A"),
+    INATIVO("I");
+
+    private String codigo;
+
+    StatusOrcamento(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public static StatusOrcamento getStatus(String codigo) {
+        for (StatusOrcamento statusOrcamento : StatusOrcamento.values()) {
+            if (statusOrcamento.getCodigo().equals(codigo)) {
+                return statusOrcamento;
+            }
+        }
+        return null;
+    }
 }
