948a78132d7a53beeb5ab616138ff780d9093bd2
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-11-04 18:43:11 -0300

    refactor: atualizando para liberar para todas as url

--- Sprint4/sprint4/src/main/java/com/example/config/CorsFilter.java
+++ Sprint4/sprint4/src/main/java/com/example/config/CorsFilter.java
@@ -11,7 +11,7 @@ public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
