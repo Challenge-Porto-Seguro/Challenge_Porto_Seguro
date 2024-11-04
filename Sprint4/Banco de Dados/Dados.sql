9b950c0d4982a31b0e3652c0dd170cdb277b4b22
ThiagoThmaz <kasuma17satto@gmail.com>
2024-11-04 20:16:01 -0300

    FEAT: Final commit Database

--- Sprint4/Banco de Dados/Dados.sql
+++ Sprint4/Banco de Dados/Dados.sql
@@ -19,6 +19,43 @@ DROP TABLE t_ps_produto CASCADE CONSTRAINTS;
 
 DROP TABLE t_ps_usuario CASCADE CONSTRAINTS;
 
+-- DROP CASCADE
+
+DROP TABLE t_ps_automovel CASCADE CONSTRAINTS;
+
+DROP TABLE t_ps_diagnostico CASCADE CONSTRAINTS;
+
+DROP TABLE t_ps_endereco CASCADE CONSTRAINTS;
+
+DROP TABLE t_ps_itens_orcamento CASCADE CONSTRAINTS;
+
+DROP TABLE t_ps_oficina CASCADE CONSTRAINTS;
+
+DROP TABLE t_ps_orcamento CASCADE CONSTRAINTS;
+
+DROP TABLE t_ps_pessoa CASCADE CONSTRAINTS;
+
+DROP TABLE t_ps_produto CASCADE CONSTRAINTS;
+
+DROP TABLE t_ps_usuario CASCADE CONSTRAINTS;
+
+DROP SEQUENCE sq_t_ps_diagnostico;
+
+DROP SEQUENCE sq_t_ps_cd_pessoa;
+
+DROP SEQUENCE sq_t_ps_oficina;
+
+DROP SEQUENCE sq_t_ps_orcamento;
+
+DROP SEQUENCE sq_t_ps_produto;
+
+DROP SEQUENCE sq_t_ps_usuario;
+
+DROP SEQUENCE sq_t_ps_automovel;
+
+DROP SEQUENCE sq_t_ps_cd_endereco;
+
+
 
 CREATE TABLE t_ps_automovel (
     cd_automovel      NUMBER(10) NOT NULL,
